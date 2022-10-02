b101d8fcab465cb06905d0cb1af6ef803240aad6
Vimal Raveendran <vimalraveendra@gmail.com>
2022-10-02 13:33:45 +0200

    added graphql library,updated Home component to fetch categories from backend using graphql,& added apollo client, apolloprovider and wrapped the component with apolloProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+
+import { Route, Routes, useParams } from "react-router-dom";
 import Navigation from "./Routes/Navigation/Navigation.component";
 import Home from "./Routes/Home /Home.component";
+export const All = () => {
+  return <div>all component</div>;
+};
+
 function App() {
+  const params = useParams();
+  console.log("params", params);
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="all" element={<All />} />
       </Route>
     </Routes>
   );
